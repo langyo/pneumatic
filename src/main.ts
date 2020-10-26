7fdc72592df8345c320b1ed794a4f2238bd098b3
langyo <langyo.china@gmail.com>
2020-10-26 23:19:14 +0800

    :factory: Clean files.

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,39 @@
 /// <reference types="node" />
 
+import { join } from 'path';
+import { Writable } from 'stream';
 import * as chalk from 'chalk';
-
 import * as Koa from 'koa';
 import * as bodyParserMiddleware from 'koa-bodyparser';
+
 import {
-  IRequestForwardObjectType
+  vmLoader,
+  webpackCompiler,
+  dirWatcher
 } from 'nickelcat-dev-server';
 
-import { build, send } from 'nickelcat-dev-server/vmLoader';
-import { webpackCompilerFactory } from 'nickelcat-dev-server/webpackLoader';
-import { staticDepsBuilder } from 'nickelcat-dev-server/staticDepsBuilder';
-import { dirWatcher } from 'nickelcat-dev-server/dirWatcher';
+let routeTasks: { match: RegExp, call: (stream: Writable) => Promise<void> }[] = [];
+let staticSPACodes: { [route: string]: string } = {};
 
-import { join } from 'path';
+function parseRoute() { }
+function parseComponent() { }
+function parseService() { }
+function routeStatic() { }
+function routeStaticComponent() { }
 
-let clientBundleContent: string = '';
+dirWatcher(
+  join(process.cwd(), './public'),
+  async (diffPaths: { path: string, type: string }[]) => {
 
-const webpackClientSideFunc = webpackCompilerFactory(
-  join(process.cwd(), './src/SPAEntry.ts'),
-  'web'
+  }
 );
-const webpackServerSideFunc = webpackCompilerFactory(
-  join(process.cwd(), './src/SSREntry.ts'),
-  'node'
-);
-
-dirWatcher(async () => {
-  clientBundleContent = (await webpackClientSideFunc(staticDepsBuilder(
-    join(process.cwd(), './src/')
-  ))).code;
-  build(await webpackServerSideFunc(staticDepsBuilder(
-    join(process.cwd(), './src/')
-  )));
-});
 
 const app = new Koa();
 
 app.use(bodyParserMiddleware());
 
 app.use(async (ctx: Koa.BaseContext, next: () => Promise<any>) => {
-  const { status, code, type, body }: IRequestForwardObjectType =
-    await send({
-      ip: ctx.ip,
-      path: ctx.path,
-      query: ctx.query,
-      host: ctx.host,
-      protocol: ctx.protocol,
-      cookies: {
-        // BUG - The type declaration file has not defined 'cookies'
-        get: (ctx as any).cookies.get,
-        set: (ctx as any).cookies.set
-      }
-    });
 
-  if (status === 'processed') {
-    ctx.type = type;
-    ctx.body = body;
-    ctx.status = code;
-  }
   await next();
 });
 
