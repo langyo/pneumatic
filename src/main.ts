019549c44db029532be00b5562b7692b5c47d6d2
langyo <langyo.china@gmail.com>
2020-11-08 23:46:29 +0800

    :bug: Add some export value checker.

--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,32 @@ watchFiles(
   ignored: /(node_modules)|(\.git)/
 }).on('all', async (type, filePath) => {
   if (['add', 'change', 'unlink'].indexOf(type) < 0) { return; }
-
   const ref = require(filePath);
+
+  // Check path and protocol.
+  if (typeof ref.path !== 'undefined' && typeof ref.path !== 'string') {
+    reporter.parseCrashReport(
+      'frontend', '[unknown]', 'The path must be a string.'
+    );
+    return;
+  }
+  if (typeof ref.protocol !== 'undefined' && typeof ref.protocol !== 'string') {
+    reporter.parseCrashReport(
+      'frontend', '[unknown]', 'The protocol must be a string.'
+    );
+    return;
+  }
+
+  // Parse path and protocol.
   const path = typeof ref.path !== 'undefined' ? ref.path
     : /^\.?(.+)(\.[a-z]+)$/.exec(filePath
       .substr(join(process.cwd(), './public/frontend').length)
       .split(/[\\\/]/)
       .join('.'))[1];
-
+  const protocol = typeof ref.protocol !== 'undefined' ? ref.protocol
+    : '' /* TODO */;
   reporter.parseEnterReport('frontend', path);
+
 });
 
 // Watch the backend part.
@@ -29,15 +46,32 @@ watchFiles(
   ignored: /(node_modules)|(\.git)/
 }).on('all', async (type, filePath) => {
   if (['add', 'change', 'unlink'].indexOf(type) < 0) { return; }
-
   const ref = require(filePath);
+
+  // Check path and protocol.
+  if (typeof ref.path !== 'undefined' && typeof ref.path !== 'string') {
+    reporter.parseCrashReport(
+      'frontend', '[unknown]', 'The path must be a string.'
+    );
+    return;
+  }
+  if (typeof ref.protocol !== 'undefined' && typeof ref.protocol !== 'string') {
+    reporter.parseCrashReport(
+      'frontend', '[unknown]', 'The protocol must be a string.'
+    );
+    return;
+  }
+
+  // Parse path and protocol.
   const path = typeof ref.path !== 'undefined' ? ref.path
     : /^\.?(.+)(\.[a-z]+)$/.exec(filePath
       .substr(join(process.cwd(), './public/backend').length)
       .split(/[\\\/]/)
       .join('.'))[1];
-
+  const protocol = typeof ref.protocol !== 'undefined' ? ref.protocol
+    : '' /* TODO */;
   reporter.parseEnterReport('backend', path);
+
 });
 
 // Watch the static files' folder.
