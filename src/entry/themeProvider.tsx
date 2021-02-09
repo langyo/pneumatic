6c76803af7c4e5a56776d2cd406fc9732ac59557
langyo <langyo.china@gmail.com>
2021-02-10 00:07:04 +0800

    :zap: Add the background.

--- src/entry/themeProvider.tsx
+++ src/entry/themeProvider.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { css } from '@emotion/css';
 
 export const ThemeProviderContext = createContext({});
 
@@ -7,10 +8,21 @@ export function ThemeProvider(props) {
   const media = useMediaQuery({
     query: '(min-width: 992px)'
   }) ? 'desktop' : 'mobile';
+  const [backgroundUrl, setBackgroundUrl] = useState(
+    'https://i.loli.net/2021/02/09/8vaVgHdnXtEwQDs.jpg'
+  );
 
   return <ThemeProviderContext.Provider value={{
     media
   }}>
+    <div className={css`
+      z-index: -1;
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      background: url(${backgroundUrl}) no-repeat top left scroll;
+      background-size: cover;
+    `} />
     {props.children}
   </ThemeProviderContext.Provider>
 }
