5f0afac96bc210884be6743161205a0c9ddfbb9a
langyo <langyo.china@gmail.com>
2021-02-28 21:40:22 +0800

    :construction: Use chokidar.

--- src/webpack.ts
+++ src/webpack.ts
@@ -6,9 +6,10 @@ import { Volume, IFs } from 'memfs';
 import { Union } from 'unionfs'
 import * as realFs from 'fs';
 import { Script, createContext } from 'vm';
-
-import { v4 } from 'uuid';
+import { v4 as generateId } from 'uuid';
+import { watch as watchFiles } from 'chokidar';
 import { EventEmitter } from 'events';
+
 import { log } from './utils/backend/logger';
 
 const globalConfig = {
@@ -47,8 +48,8 @@ const globalConfig = {
   }
 }
 
-let clientBundleId = {
-  main: v4()
+let clientBundleRouteId = {
+  main: generateId()
 };
 
 const clientSideFs: IFs = ((new Union()) as any).use(realFs).use(Volume.fromJSON({
@@ -66,24 +67,7 @@ document.querySelector('#root')
 }));
 clientSideFs['join'] = join;
 
-const clientSideCompiler = webpack({
-  ...globalConfig,
-  entry: {
-    main: join(process.cwd(), './main.ts')
-  },
-  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-  output: {
-    filename: '[name].bundle.js',
-    path: process.cwd()
-  },
-  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map'
-});
-clientSideCompiler.inputFileSystem = clientSideFs;
-clientSideCompiler.outputFileSystem = clientSideFs;
-
-clientSideCompiler.watch({
-  ignored: ['**/node_modules/**', '**/.git/**']
-}, (err: Error, status) => {
+function clientSideCompilerCallback(err: Error, status) {
   if (err) {
     console.error(err);
   } else if (status.hasErrors()) {
@@ -103,13 +87,13 @@ clientSideCompiler.watch({
   } else {
     log('info', 'Compiled the client part.');
   }
-});
+};
 
 export async function clientSideMiddleware(
   ctx: Koa.BaseContext,
   next: () => Promise<unknown>
 ) {
-  const entryId = clientBundleId.main;
+  const entryId = clientBundleRouteId.main;
 
   switch (ctx.path) {
     case `/${entryId}`:
@@ -137,21 +121,8 @@ export async function clientSideMiddleware(
   }
 }
 
-const serverPartFs: IFs = Volume.fromJSON({}) as IFs;
-serverPartFs['join'] = join;
-
-const serverPartCompiler = webpack({
-  ...globalConfig,
-  entry: join(__dirname, './serverEntry.ts'),
-  mode: 'development',
-  target: 'node',
-  output: {
-    filename: '[name].js',
-    path: process.cwd()
-  },
-  devtool: 'eval-source-map'
-});
-serverPartCompiler.outputFileSystem = serverPartFs;
+const serverSideFs: IFs = Volume.fromJSON({}) as IFs;
+serverSideFs['join'] = join;
 
 export let serverSideMiddleware = async (
   _ctx: Koa.BaseContext,
@@ -163,9 +134,7 @@ export let serverSideLongtermMiddleware: {
   [pkg: string]: (ctx, emitter: EventEmitter) => Promise<void>
 } = {};
 
-serverPartCompiler.watch({
-  ignored: ['**/node_modules/**', '**/.git/**']
-}, (err: Error, status) => {
+function serverSideCompilerCallback(err: Error, status) {
   if (err) {
     console.error(err);
   } else if (status.hasErrors()) {
@@ -186,7 +155,7 @@ serverPartCompiler.watch({
     log('info', 'Compiled the service part.');
 
     const script = new Script(
-      serverPartFs.readFileSync(join(process.cwd(), '/main.js'), 'utf8') as string, {
+      serverSideFs.readFileSync(join(process.cwd(), '/main.js'), 'utf8') as string, {
       filename: 'serverEntry.js'
     });
     const context = createContext({
@@ -220,5 +189,61 @@ serverPartCompiler.watch({
     } catch (e) {
       console.error(e);
     }
+  }
+};
+
+let watcherWaitingState = {
+  firstChange: false,
+  continueChange: false
+};
+function watcherTrigger() {
+  if (!watcherWaitingState.continueChange) {
+    log('info', 'Compiling the codes.');
+    watcherWaitingState.firstChange = false;
+    watcherWaitingState.continueChange = false;
+
+    const clientSideCompiler = webpack({
+      ...globalConfig,
+      entry: {
+        main: join(process.cwd(), './main.ts')
+      },
+      mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+      output: {
+        filename: '[name].bundle.js',
+        path: process.cwd()
+      },
+      devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map'
+    });
+    clientSideCompiler.inputFileSystem = clientSideFs;
+    clientSideCompiler.outputFileSystem = clientSideFs;
+    clientSideCompiler.run(clientSideCompilerCallback);
+
+    const serverSideCompiler = webpack({
+      ...globalConfig,
+      entry: join(__dirname, './serverEntry.ts'),
+      mode: 'development',
+      target: 'node',
+      output: {
+        filename: '[name].js',
+        path: process.cwd()
+      },
+      devtool: 'eval-source-map'
+    });
+    serverSideCompiler.outputFileSystem = serverSideFs;
+    serverSideCompiler.run(serverSideCompilerCallback);
+
+  } else {
+    watcherWaitingState.continueChange = false;
+    setTimeout(watcherTrigger, 3000);
+  }
+}
+watchFiles(__dirname, {
+  ignored: /^(node_modules)|(\.git)$/
+}).on('all', (_event, path) => {
+  if (!watcherWaitingState.firstChange) {
+    watcherWaitingState.firstChange = true;
+    setTimeout(watcherTrigger, 3000);
+  } else {
+    watcherWaitingState.continueChange = true;
   }
 });
